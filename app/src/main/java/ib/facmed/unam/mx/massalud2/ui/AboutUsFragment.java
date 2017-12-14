a4d0c237905e41851fbedba414b320809264b372
Samo92 <damianmaciel92@gmail.com>
2017-12-13 22:54:43 -0600

    first commit

---
+++ app/src/main/java/ib/facmed/unam/mx/massalud2/ui/AboutUsFragment.java
@@ -0,0 +1,30 @@
+package ib.facmed.unam.mx.massalud2.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ib.facmed.unam.mx.massalud2.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AboutUsFragment extends Fragment {
+
+
+    public AboutUsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_aboutus, container, false);
+    }
+
+}
